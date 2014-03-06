6450bf6fb3a94bfc4cde1c22b4c3e8a4be1b938a
Martijn <mvancooten@davinci.nl>
2014-03-06 10:04:39 +0100

    basic rectangle

---
+++ src/rechthoek/rechthoek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rechthoek
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("┌┐");
+            Console.WriteLine("└┘");
+            Console.Read();
+        }
+
+        
+    }
+}
